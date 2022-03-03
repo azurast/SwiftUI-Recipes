0516f7a7518ed608279ad88989e04240f206210c
Azura Sakan Taufik <azstima@gmail.com>
2022-03-03 09:07:04 +0700

    Initial Commit

---
+++ SwiftUI Recipes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Recipes
+//
+//  Created by TI Digital on 03/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI Recipes/SwiftUI_RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_RecipesApp.swift
+//  SwiftUI Recipes
+//
+//  Created by TI Digital on 03/03/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
